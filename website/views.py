cdedab9c627319de52c6acefaaceb8c6c0e20149
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-05-09 14:13:23 -0300

    add: Navbar, Funções de Login e Autenticação

--- website/views.py
+++ website/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    # Checar se o usuário está logado
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Autenticação
+        user = authenticate(request, username=username, password=password)  # use o email em vez do nome de usuário
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Você logou com sucesso!")
+            return redirect('home')
+        else:
+            messages.error(request, "Erro: Cheque suas crendenciais e me forneça de forma correta")
+            return redirect('home')
+    return render(request, 'home.html', {})
+
+# def login_user(request):
+#    pass
+
+# def logout_user(request):
+#    pass
